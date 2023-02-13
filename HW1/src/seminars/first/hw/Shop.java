c251ce53a8cddf2a63518ba241c02dd408684b51
caregor <caregor@gmail.com>
2023-02-13 17:06:49 +0500

    release 2 methods getSortedListProducts and getMostExpensiveProduct;

--- HW1/src/seminars/first/hw/Shop.java
+++ HW1/src/seminars/first/hw/Shop.java
@@ -1,5 +1,7 @@
 package seminars.first.hw;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Shop {
@@ -17,14 +19,21 @@ public class Shop {
     /**
      * @return отсортированный по возрастанию и цене список продуктов
      */
-    public List<Product> getSortedListProducts() {
-        return null;
+    public List<Product> getSortedListProducts(List<Product> products) {
+        List<Product> sortedProducts = new ArrayList<>(products);
+        Collections.sort(sortedProducts, (product1, product2) -> Integer.compare(product1.getCost(), product2.getCost()));
+        return sortedProducts;
     }
 
     /**
      * @return самый дорогой продукт
      */
-    public Product getMostExpensiveProduct() {
-        return null;
+    public Product getMostExpensiveProduct(List<Product> products) {
+        Product lastItem = null;
+        List<Product> sortedProducts = getSortedListProducts(products);
+        for (Product item:sortedProducts) {
+            lastItem = item;
+        }
+        return lastItem;
     }
 }
